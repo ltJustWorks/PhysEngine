9c747520882a37b14da7184b50e438bbc1438a2f
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-23 23:08:46 -0500

    collision handling

--- main.cpp
+++ main.cpp
@@ -20,19 +20,40 @@ void updateVelocity(Particle *p, double ax, double ay, double az, double dt) {
     p->vz += az * dt;
 }
 
+void handleWallCollision(Particle *p, double boxSize, double restitution) {
+    // Bounce off walls
+    if (p->x < 0 || p->x > boxSize) {
+        p->vx *= -restitution;
+        if (p->x < 0) p->x = 0;
+        else p->x = boxSize;
+    }
+    if (p->y < 0 || p->y > boxSize) {
+        p->vy *= -restitution;
+        if (p->y < 0) p->y = 0;
+        else p->y = boxSize;
+    }
+    if (p->z < 0 || p->z > boxSize) {
+        p->vz *= -restitution;
+        if (p->z < 0) p->z = 0;
+        else p->z = boxSize;
+    }
+}
+
 int main() {
     Particle particle;
 
     // Initial conditions
-    particle.x = 0.0;
-    particle.y = 0.0;
-    particle.z = 0.0;
+    particle.x = 5.0;
+    particle.y = 5.0;
+    particle.z = 5.0;
     particle.vx = 1.0; // Initial velocity along the x-axis
-    particle.vy = 0.0;
-    particle.vz = 0.0;
+    particle.vy = 0.5; // Initial velocity along the y-axis
+    particle.vz = -0.5; // Initial velocity along the z-axis
 
     double dt = 0.1; // Time step
-    double totalTime = 20.0; // Total simulation time
+    double totalTime = 30.0; // Total simulation time
+    double boxSize = 10.0; // Size of the bounding box
+    double restitution = 0.9; // Coefficient of restitution
 
     FILE *outputFile = fopen("C:\\temp\\trajectory.dat", "w");
     if (outputFile == nullptr) {
@@ -51,6 +72,7 @@ int main() {
 
         updateVelocity(&particle, ax, ay, az, dt);
         updatePosition(&particle, dt);
+        handleWallCollision(&particle, boxSize, restitution);
 
         fprintf(outputFile, "%.2f\t%.2f\t%.2f\t%.2f\n", t, particle.x, particle.y, particle.z);
     }
